c7889756e2889afd0c68b68708c0957baff9f2b8
Marc Neuhaus <mneuhaus@famelo.com>
2014-02-03 14:42:15 +0100

    Added CSV Export

--- Classes/Famelo/Satisfy/Controller/ExportController.php
+++ Classes/Famelo/Satisfy/Controller/ExportController.php
@@ -2,7 +2,7 @@
 namespace Famelo\Satisfy\Controller;
 
 /*                                                                        *
- * This script belongs to the TYPO3 Flow package "Famelo.Satisfy".            *
+ * This script belongs to the TYPO3 Flow package 'Famelo.Satisfy".            *
  *                                                                        *
  *                                                                        */
 
@@ -23,6 +23,40 @@ class ExportController extends \TYPO3\Flow\Mvc\Controller\ActionController {
 		$document->assign('survey', $survey);
 		// TODO: String to -> Settings!
 		$document->download('Satisfy Bewertung ' . $survey->getCreated()->format('d.m.Y') . '.pdf');
+	}
+
+	/**
+	 * @param \Famelo\Satisfy\Domain\Model\Campaign $campaign
+	 * @return void
+	 */
+	public function campaignCsvAction($campaign) {
+		$rows = array();
+		foreach ($campaign->getMailSurveys() as $survey) {
+			$row = array(
+				'Firma' => $survey->getContact()->getCustomer(),
+				'Ansprechpartner' => $survey->getContact()->__toString(),
+				'E-Mail' => $survey->getContact()->getEmail()
+			);
+
+			foreach ($survey->getResults() as $result) {
+				$row[$result->getQuestion()] = $result->getData();
+			}
+
+			$rows[] = $row;
+		}
+
+		$output = fopen('php://output', 'w');
+		header('Cache-Control: private', false);
+		header('Content-Type: application/octet-stream');
+		header('Content-Disposition: attachment; filename="' . $campaign->getName() . '.csv";');
+		header('Content-Transfer-Encoding: binary');
+		fputcsv($output, array_keys(reset($rows)), ';');
+		foreach ($rows as $row) {
+			fputcsv($output, $row, ';');
+		}
+		fclose($output);
+
+		exit();
 	}
 }
 
